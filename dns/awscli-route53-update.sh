a4d0012d85a8eacfc63a7d9fac3f5ef8bcf64071
melt <melt@getpimp.org>
2019-07-05 12:28:21 -0400

    script to update amazon dns

---
+++ dns/awscli-route53-update.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+# Work & Data directory
+DIR="/opt/awsdns"
+
+cd ${DIR}
+
+# Hosted Zone ID or the Domain to manage in AWS
+ZONEID="ZCJCUP3N0VDYG"
+
+# The CNAME you want to update e.g. hello.example.com
+RECORDSET="edmonds.skyhighatry.com."
+TTL=300
+COMMENT="Auto updating @ `date`"
+# Change to AAAA if using an IPv6 address
+TYPE="A"
+
+# Get WAN IP
+IP="`ifconfig eth0 | awk '/inet addr/{split($2,tary,":");print tary[2];}'`"
+
+LOGFILE="$DIR/update-route53.log"
+IPFILE="$DIR/update-route53.ip"
+
+# Check if the IP has changed
+if [ ! -f "$IPFILE" ]
+then
+    touch "$IPFILE"
+fi
+
+if grep -Fxq "$IP" "$IPFILE"
+then
+    echo "IP is still $IP. Exiting" >> "$LOGFILE"
+    exit 0
+else
+    echo "IP has changed to $IP" >> "$LOGFILE"
+    # Fill a temp file with valid JSON
+    TMPFILE=${DIR}/awsdns.json
+    cat > ${TMPFILE} << EOF
+    {
+      "Comment":"$COMMENT",
+      "Changes":[
+        {
+          "Action":"UPSERT",
+          "ResourceRecordSet":{
+            "ResourceRecords":[
+              {
+                "Value":"$IP"
+              }
+            ],
+            "Name":"$RECORDSET",
+            "Type":"$TYPE",
+            "TTL":60
+          }
+        }
+      ]
+    }
+EOF
+
+    # Update the Hosted Zone record
+    aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONEID \
+        --change-batch file://"$TMPFILE" >> "$LOGFILE"
+    echo "" >> "$LOGFILE"
+
+fi
+
+# All Done - cache the IP address for next time
+echo "$IP" > "$IPFILE"
